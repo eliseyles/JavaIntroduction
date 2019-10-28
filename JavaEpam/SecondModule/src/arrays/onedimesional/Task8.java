9be34ad3f03ed10031f03c701d7feb69d0790aaf
Alesya Khoroneko <horoneko00@gmail.com>
2019-10-28 20:32:58 +0300

    add task8

---
+++ JavaEpam/SecondModule/src/arrays/onedimesional/Task8.java
@@ -0,0 +1,44 @@
+//8. Дана последовательность целых чисел а1 ,а2 ,..., аn.
+//Образовать новую последовательность, выбросив из исходной те члены, которые равны min(а1 ,а2 ,..., аn.)
+
+package arrays.onedimesional;
+
+import util.Input;
+
+import java.util.ArrayList;
+
+public class Task8 {
+    public static final int INVALIDVALUE = Integer.MIN_VALUE;
+
+    public static void main(String[] args) {
+        int[] arr = Input.nextIntArray();
+
+        System.out.println(makeArrWithoutMin(arr));
+    }
+
+    private static ArrayList<Integer> makeArrWithoutMin(int[] arr) {
+        if (arr != null) {
+            ArrayList<Integer> arrayList = new ArrayList<>();
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] != findMin(arr)) {
+                    arrayList.add(arr[i]);
+                }
+            }
+            return arrayList;
+        }
+        return null;
+    }
+
+    private static int findMin(int[] arr) {
+        if (arr != null) {
+            int min = arr[0];
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] < min) {
+                    min = arr[i];
+                }
+            }
+            return min;
+        }
+        return INVALIDVALUE;
+    }
+}
