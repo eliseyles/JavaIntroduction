2411a5b2788977eee8a4f45075648b5e44ffe9c1
Alesya Khoroneko <horoneko00@gmail.com>
2019-11-11 13:42:37 +0300

    added Task8

---
+++ JavaEpam/ThirdModule/src/strings/stringobject/Task8.java
@@ -0,0 +1,47 @@
+//8. Вводится строка слов, разделенных пробелами. Найти самое длинное слово и вывести его на экран. Случай, когда самых
+//длинных слов может быть несколько, не обрабатывать.
+
+package strings.stringobject;
+
+import strings.util.Input;
+
+public class Task8 {
+    private static final String EMPTY_STRING = "";
+    private static final int INDEX_NON_INSIDE = -1;
+    private static final String SPACE = " ";
+    private static final int OFFSET = 1;
+
+
+    public static void main(String[] args) {
+        String string = Input.nextLine("Enter string: ");
+        System.out.println(findLongestWord(string));
+    }
+
+    public static String findLongestWord(String string) {
+        if (string != null) {
+            String longestWord = EMPTY_STRING;
+            int spacePosition = string.indexOf(SPACE);
+            while (spacePosition != INDEX_NON_INSIDE) {
+                if (spacePosition + OFFSET < string.length()) {
+                    if (string.indexOf(SPACE, spacePosition + OFFSET) != INDEX_NON_INSIDE) {
+                        if (longestWord.length()
+                                < string.substring(spacePosition + OFFSET,
+                                string.indexOf(SPACE, spacePosition + OFFSET)).length()) {
+
+                            longestWord = string.substring(spacePosition + OFFSET,
+                                    string.indexOf(SPACE, spacePosition + OFFSET));
+                        }
+                    } else {
+                        if (longestWord.length() < string.substring(spacePosition + OFFSET).length()) {
+                            longestWord = string.substring(spacePosition + OFFSET);
+                        }
+                    }
+                }
+                spacePosition = string.indexOf(SPACE, spacePosition + OFFSET);
+            }
+            return longestWord;
+
+        }
+        return EMPTY_STRING;
+    }
+}
